aec3b0afd694080ff5e7940ef93f1911a0f6b3c5
Esther Weon <esther.s.weon@gmail.com>
2017-12-19 09:50:09 -0800

    Feedback

--- seed.js
+++ seed.js
@@ -1,31 +1,31 @@
 var db = require('./models');
 
 var cities_list = [
-{
-	name: "New Delhi, India",
-	description: "I grew up here before immigrating."
-}, 
-{
-	name: "Indianapolis, Indiana",
-	description: "This is the first place I lived in America."
-}, 
-{
-	name: "Solon, Ohio",
-	description: "I graduated high school here."
-}, 
-{
-	name: "San Francisco, California",
-	description: "I live here now."
-}
-]
+  {
+  	name: "New Delhi, India",
+  	description: "I grew up here before immigrating."
+  }, 
+  {
+  	name: "Indianapolis, Indiana",
+  	description: "This is the first place I lived in America."
+  }, 
+  {
+  	name: "Solon, Ohio",
+  	description: "I graduated high school here."
+  }, 
+  {
+  	name: "San Francisco, California",
+  	description: "I live here now."
+  }
+];
 
 db.City.remove({}, function(err, cities){
-  // code in here runs after all cities are removed
+  console.log("all cities removed");
+
   db.City.create(cities_list, function(err, cities){
-    // code in here runs after all cities are created
-    if (err) { return console.log('ERROR', err); }
-    console.log("all albums:", cities);
-    console.log("created", cities.length, "cities");
+    if (err) { console.log('ERROR: ', err); }
+    console.log("all cities: ", cities);
+    console.log("created ", cities.length, " cities");
     process.exit();
   });
 });
